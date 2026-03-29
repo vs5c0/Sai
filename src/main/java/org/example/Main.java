59be7491720b0e24e99cf7fb0f2812dcf954836c
vs5c0 <venkatsai7722@gmail.com>
2026-03-29 22:28:07 +0530

    Feactures changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,11 @@ public class Main {
         System.out.print("Hello world!");
         System.out.println("Feacture updated Important");
 
+
+        System.out.print("Hello worlds!");
+        System.out.println("Feactures updated Important");
+        System.out.println("Feacture updated Important");
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
